bac1084c0aa8bc500ff68e98678d4aaffcefacf8
Braeden Kilburn <braeden.kilburn@vivint.com>
2025-03-31 18:27:03 -0700

    chore: update deps

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from '@/router'
+import { createGtag } from 'vue-gtag'
 
 import PrimeVue from 'primevue/config'
 import Lara from '@primeuix/themes/lara'
 import 'primeicons/primeicons.css'
-
 import Card from 'primevue/card'
 import FloatLabel from 'primevue/floatlabel'
 import InputText from 'primevue/inputtext'
@@ -14,20 +14,16 @@ import Button from 'primevue/button'
 import ToastService from 'primevue/toastservice'
 import Dialog from 'primevue/dialog'
 
-import VueGtag from 'vue-gtag'
-
 import '@/assets/main.scss'
 
 const app = createApp(App)
 app.use(router)
 
-app.use(
-  VueGtag,
-  {
-    config: { id: import.meta.env.VITE_GTAG_ID }
-  },
-  router
-)
+const gtag = createGtag({
+  tagId: import.meta.env.VITE_GTAG_ID,
+  pageTracker: { router },
+})
+app.use(gtag)
 
 const pinia = createPinia()
 app.use(pinia)
